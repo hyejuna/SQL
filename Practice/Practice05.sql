5d8d2d9b90b99a00115890e27371f7c4236f894d
나혜주 <he_ju0609@naver.com>
2021-12-24 23:27:36 +0900

    Practice05 완료

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -86,3 +86,66 @@ from employees e, departments d
 where hire_date = ( select max(hire_date)
                  from employees)
 and e.department_id = d.department_id;
+
+
+--문제7
+select  e.employee_id 사번,
+        e.first_name 이름,
+        e.last_name 성,
+        e.salary 급여,
+        maxDep.avgSal avg_salary,
+        j.job_title        
+from employees e, jobs j,
+     (select department_id, 
+             avg(salary) avgSal
+      from employees
+      group by department_id
+      having avg(salary) = (select max(avg(salary))
+                            from employees
+                            group by department_id)) maxDep
+where e.department_id = maxdep.department_id
+and e.job_id = j.job_id;
+
+
+--문제8
+select  d.department_name       
+from departments d,
+     (select department_id, 
+             avg(salary) avgSal
+      from employees
+      group by department_id
+      having avg(salary) = (select max(avg(salary))
+                            from employees
+                            group by department_id)) maxDep
+where d.department_id = maxdep.department_id;
+
+
+--문제9
+select ort.region_name
+from ( select  rownum rn,
+               ot.region_name,
+               ot.avgsal
+       from (select  avg(e.salary) avgSal,
+                     r.region_name
+             from employees e, departments d, locations l, countries c, regions r
+             where e.department_id = d.department_id
+             and d.location_id = l.location_id
+             and l.country_id = c.country_id
+             and c.region_id = r.region_id
+             group by r.region_name
+             order by avg(e.salary) desc ) ot)ort
+where rn = 1;
+
+
+--문제10
+select ort.job_title
+from(select  rownum rn,
+             ot.job_title,
+             ot.avgsal
+     from (select j.job_title, 
+                  avg(e.salary) avgSal
+           from employees e, jobs j
+           where e.job_id = j.job_id
+           group by j.job_title
+           order by avg(e.salary) desc) ot) ort
+where rn = 1;
